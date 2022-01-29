c640665f30ee3410e2c2e12714b2a5a0ca1be291
TDB-star <tatiana.blache@gmail.com>
2022-01-29 14:06:30 +0100

    Add networking in ti AccountSummary

---
+++ MyPersonalBank/Utils/Date+Util.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Util.swift
+//  MyPersonalBank
+//
+//  Created by Tatiana Dmitrieva on 28/01/2022.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
